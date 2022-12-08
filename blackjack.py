62151e468206c894a3ab576726ca976f770e9ab3
Esther Wang <70925047+estherwng@users.noreply.github.com>
2022-12-08 00:16:00 -0500

    Add files via upload

---
+++ blackjack.py
@@ -0,0 +1,31 @@
+import random
+
+FACE_CARD_VALUE = 10
+ACE_VALUE = 1
+CARD_LABELS = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+BLACKJACK = 21
+DEALER_THRESHOLD = 16
+
+
+####### DO NOT EDIT ABOVE ########
+
+def main():
+    """Runs a program for playing Blackjack with one player
+    and a dealer
+    """
+
+    # call play_blackjack() here and remove pass below
+    pass
+
+
+####### DO NOT REMOVE IF STATEMENT BELOW ########
+
+if __name__ == "__main__":
+    #Remove comments for next 4 lines to run doctests
+    #print("Running doctests...")
+    #import doctest
+    #doctest.testmod(verbose=True)
+
+    #print("\nRunning program...\n")
+
+    main()
